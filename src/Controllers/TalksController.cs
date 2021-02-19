4842d2bb965f87103a325813e3be4b4ed944d3a3
Qing Ma <gsmofgsm@hotmail.com>
2021-02-19 13:04:06 +0100

    Get a talk

--- src/Controllers/TalksController.cs
+++ src/Controllers/TalksController.cs
@@ -40,5 +40,19 @@ namespace CoreCodeCamp.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, "Failed to get Talks");
             }
         }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<TalkModel>> Get(string moniker, int id)
+        {
+            try
+            {
+                var talks = await _repository.GetTalkByMonikerAsync(moniker, id);
+                return _mapper.Map<TalkModel>(talks);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to get Talks");
+            }
+        }
     }
 }
