e6c09a3c5b914c66417889fa5e691b253804f146
Qing Ma <gsmofgsm@hotmail.com>
2021-02-19 10:55:15 +0100

    DELETE a camp

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -144,5 +144,28 @@ namespace CoreCodeCamp.Controllers
 
             return BadRequest();
         }
+
+        [HttpDelete("{moniker}")]
+        public async Task<ActionResult<CampModel>> Delete(string moniker)
+        {
+            try
+            {
+                var oldCamp = await _repository.GetCampAsync(moniker);
+                if (oldCamp == null) return NotFound($"Could not find camp with moniker of {moniker}");
+
+                _repository.Delete(oldCamp);
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    return Ok();
+                }
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+
+            return BadRequest($"Failed deleting Camp with moniker {moniker}");
+        }
     }
 }
