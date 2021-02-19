8fa26d4ba4131907a8e3f4e09b2bcd8dd01e3845
Qing Ma <gsmofgsm@hotmail.com>
2021-02-19 13:43:46 +0100

    Create a talk

--- src/Data/CampProfile.cs
+++ src/Data/CampProfile.cs
@@ -15,8 +15,10 @@ namespace CoreCodeCamp.Data
             this.CreateMap<Camp, CampModel>()
                 .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName))
                 .ReverseMap() ;
-            this.CreateMap<Talk, TalkModel>();
-            this.CreateMap<Speaker, SpeakerModel>();
+            this.CreateMap<Talk, TalkModel>()
+                .ReverseMap();
+            this.CreateMap<Speaker, SpeakerModel>()
+                .ReverseMap();
             //this.CreateMap<CampModel, Camp>()
             //    .ForPath(c => c.Location.VenueName, m => m.MapFrom(o => o.Venue));
         }
